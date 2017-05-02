20d7e38da276d33af902a3d76c965c1d1ec8daec
Fatming <osimejp@yahoo.co.jp>
2017-05-02 10:29:26 +0800

    delete useless files

--- Src/FM79979Engine/AndroidProject/FacebookSDK/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\leeyo\Desktop\FM79979\Src\FM79979Engine\AndroidProject\FacebookSDK\bin\AndroidManifest.xml : \
-C:\Users\leeyo\Desktop\FM79979\Src\FM79979Engine\AndroidProject\FacebookSDK\AndroidManifest.xml \
