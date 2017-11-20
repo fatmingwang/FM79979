fd5659438f3703da125facc89287cc41dd4549a8
Fatming <osimejp@yahoo.co.jp>
2017-11-20 15:38:17 +0800

    add language file support

---
+++ Src/FM79979Engine/Core/GameplayUT/Language/LanguageFile.cpp
@@ -0,0 +1,140 @@
+#include "../../stdafx.h"
+#include "LanguageFile.h"
+
+namespace FATMING_CORE
+{
+	//<cXMLFileList Name="CH">
+	//	<File Key="KeyName" FileName="somewhere/filename.extension" />
+	//</cXMLFileList>
+
+	cXMLFileList::cXMLFileList()
+	{
+
+	}
+	cXMLFileList::~cXMLFileList()
+	{
+		DEL_MAP(std::wstring, std::wstring*, m_KeyAndFileNameMap);
+	}
+	bool	cXMLFileList::MyParse(TiXmlElement*e_pRoot)
+	{
+		DEL_MAP(std::wstring, std::wstring*, m_KeyAndFileNameMap);
+		//const wchar_t*l_strName = e_pRoot->Attribute(L"Name");
+		//if(!l_strName)
+		//{ 
+		//	UT::ErrorMsg("cXMLFileList dont has a name!", m_strFileName);
+		//	return false;
+		//}
+		//this->SetName(l_strName);
+		e_pRoot = e_pRoot->FirstChildElement();
+		while (e_pRoot)
+		{
+			const wchar_t*l_strElementValue = e_pRoot->Value();
+			if(wcscmp(l_strElementValue,L"File") == 0)
+			{
+				std::wstring*l_pstrFileName = nullptr;
+				std::wstring l_strKey;
+				PARSE_ELEMENT_START(e_pRoot)
+					COMPARE_NAME("Key")
+					{
+						l_strKey = l_strValue;
+					}
+					else
+					COMPARE_NAME("FileName")
+					{
+						l_pstrFileName = new std::wstring(l_strValue);
+					}
+				PARSE_NAME_VALUE_END
+				assert(l_pstrFileName &&"FileName no data!?");
+				if (m_KeyAndFileNameMap.find(l_strKey) != m_KeyAndFileNameMap.end())
+				{
+					UT::ErrorMsg(L"Error Key exists!", l_strName);
+				}
+				m_KeyAndFileNameMap.insert(std::make_pair(l_strKey, l_pstrFileName));
+			}
+			e_pRoot = e_pRoot->NextSiblingElement();
+		}
+		return true;
+	}
+		
+
+	std::wstring*		cXMLFileList::GetFileName(const wchar_t*e_strKey)
+	{
+		auto l_Iterator = m_KeyAndFileNameMap.find(e_strKey);
+		if (l_Iterator != m_KeyAndFileNameMap.end())
+			return l_Iterator->second;
+		return nullptr;
+	}
+
+
+	cLanguageFile::cLanguageFile(const char*e_strSetupFile)
+	{
+		m_strDefaultLanguage = L"";
+		this->ParseWithMyParse(e_strSetupFile);
+		//m_strDefaultLanguage = m_strCurrentLanguage = e_strDefaultLanguage;
+		assert(m_strDefaultLanguage.length() > 0 && "default language wrong!?");
+	}
+	cLanguageFile::~cLanguageFile()
+	{
+
+	}
+	//<cLanguageFile Default="CH">
+	//<Language Name="" FileName=""/>
+	//</cLanguageFile>
+	bool	cLanguageFile::MyParse(TiXmlElement*e_pRoot)
+	{
+		this->Destroy();
+		auto l_strDefault = e_pRoot->Attribute(L"Default");
+		if (l_strDefault)
+		{
+			m_strCurrentLanguage = this->m_strDefaultLanguage = l_strDefault;
+		}
+		e_pRoot = e_pRoot->FirstChildElement();
+		while (e_pRoot)
+		{
+			std::wstring l_strLanguageName;
+			cXMLFileList*l_pXMLFileList = nullptr;
+			PARSE_ELEMENT_START(e_pRoot)
+				COMPARE_NAME("Name")
+				{
+					l_strLanguageName = l_strValue;
+				}
+				else
+				COMPARE_NAME("FileName")
+				{
+					l_pXMLFileList = new cXMLFileList();
+					if (!l_pXMLFileList->ParseWithMyParse(l_strValue))
+					{
+						UT::ErrorMsg(L"language parse failed!", l_strValue);
+					}
+				}
+			PARSE_NAME_VALUE_END
+			l_pXMLFileList->SetName(l_strLanguageName);
+			if (!this->AddObject(l_pXMLFileList))
+			{
+				SAFE_DELETE(l_pXMLFileList);
+				UT::ErrorMsg(L"same language!", l_strLanguageName.c_str());
+			}
+			e_pRoot = e_pRoot->NextSiblingElement();
+		}
+		return true;
+	}
+	std::wstring*		cLanguageFile::GetFileName(const wchar_t*e_strKey)
+	{
+		auto l_pObject = this->GetObject(m_strCurrentLanguage.c_str());
+		if (l_pObject)
+		{
+			auto l_pstrFeilName = l_pObject->GetFileName(e_strKey);
+			if (l_pstrFeilName)
+			{
+				return l_pstrFeilName;
+			}
+		}
+		l_pObject = this->GetObject(m_strDefaultLanguage.c_str());
+		assert(l_pObject &&"no default object!?");
+		if(l_pObject)
+			return l_pObject->GetFileName(e_strKey);
+		return nullptr;
+	}
+
+//end namespace FATMING_CORE
+}
---
+++ Src/FM79979Engine/Core/GameplayUT/Language/LanguageFile.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "../../XML/XMLLoader.h"
+
+namespace FATMING_CORE
+{
+	//<cXMLFileList Name="CH">
+	//	<File Key="KeyName" FileName="somewhere/filename.extension" />
+	//</cXMLFileList>
+	class cXMLFileList :public cNodeISAX,public NamedTypedObject
+	{
+		std::map<std::wstring, std::wstring*>	m_KeyAndFileNameMap;
+		virtual	bool	MyParse(TiXmlElement*e_pRoot);
+	public:
+		cXMLFileList();
+		~cXMLFileList();
+		std::wstring*		GetFileName(const wchar_t*e_strKey);
+	};
+	//if target language cann find the file use default file.
+	//<cLanguageFile Default="CH">
+		//<Language Name="CH" FileName="CH_LanguageSetting.xml"/>
+		//<Language Name="EN" FileName="EN_LanguageSetting.xml"/>
+	//</cLanguageFile>
+	class cLanguageFile :public cNamedTypedObjectVector<cXMLFileList> , public cNodeISAX
+	{
+		std::wstring		m_strCurrentLanguage;
+		std::wstring		m_strDefaultLanguage;
+		virtual	bool		MyParse(TiXmlElement*e_pRoot);
+	public:
+		cLanguageFile(const char*e_strSetupFile);
+		~cLanguageFile();
+		const std::wstring	GetCurrentLanguage() { return m_strCurrentLanguage; }
+		void				SetCurrentLanguage(const wchar_t*e_strCurrentLanguage) { m_strCurrentLanguage = e_strCurrentLanguage; }
+		std::wstring*		GetFileName(const wchar_t*e_strKey);
+	};
+
+	//cLanguageFile l_LanguageFile("Language/Language.xml");
+	//auto l_p1 = l_LanguageFile.GetFileName(L"KeyName");
+	//l_LanguageFile.SetCurrentLanguage(L"EN");
+	//auto l_p11 = l_LanguageFile.GetFileName(L"KeyName");
+	//l_LanguageFile.SetCurrentLanguage(L"CN");
+	//auto l_p2 = l_LanguageFile.GetFileName(L"KeyName2");
+	//auto l_p3 = l_LanguageFile.GetFileName(L"KeyName4");
+//end namespace FATMING_CORE
+}
