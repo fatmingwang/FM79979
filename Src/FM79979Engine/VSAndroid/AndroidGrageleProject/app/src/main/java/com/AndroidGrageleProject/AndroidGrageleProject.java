038d7781876ea73e4d74b09ef9b36092b99e0d95
Fatming <osimejp@yahoo.co.jp>
2021-07-28 17:44:33 +0800

    update VSAndroid project

---
+++ Src/FM79979Engine/VSAndroid/AndroidGrageleProject/app/src/main/java/com/AndroidGrageleProject/AndroidGrageleProject.java
@@ -0,0 +1,22 @@
+
+package com.AndroidGrageleProject;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class AndroidGrageleProject extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        /* Create a TextView and set its text to "Hello world" */
+        TextView  tv = new TextView(this);
+        tv.setText("Hello World!");
+        setContentView(tv);
+    }
+    static { System.loadLibrary("AndroidGameApp.NativeActivity"); }
+}
