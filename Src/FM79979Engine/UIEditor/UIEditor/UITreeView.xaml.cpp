1cb7f76c7e53f9429e7745a66e4468b998b03966
Fatming <osimejp@yahoo.co.jp>
2023-01-30 22:06:19 +0800

    add cMPDINode test code

--- Src/FM79979Engine/UIEditor/UIEditor/UITreeView.xaml.cpp
+++ Src/FM79979Engine/UIEditor/UIEditor/UITreeView.xaml.cpp
@@ -9,6 +9,7 @@
 using namespace UIEditor;
 
 using namespace Platform;
+
 using namespace Windows::Foundation;
 using namespace Windows::Foundation::Collections;
 using namespace Windows::UI::Xaml;
@@ -24,17 +25,101 @@ using namespace Windows::UI::Xaml::Navigation;
 UITreeView::UITreeView()
 {
 	InitializeComponent();
+	m_pLastIvokedTreeviewNode = nullptr;
 }
 
 
+enum eMoveType
+{
+	//multi-nodes change order.
+	eMT_PROCESS_NODECHANGE_ORDER = 0,
+	eMT_PROCESS_NODE_CHANGE_PARENT,
+	eMT_MAX
+};
+//if parent treeviewitem is not at e_pCollections,it's parellel
+
+//Microsoft::UI::Xaml::Controls::TreeViewNode^ GetParentInCollections(Microsoft::UI::Xaml::Controls::TreeViewNode^e_pTarget, Windows::Foundation::Collections::IVectorView<Platform::Object^>^ e_pCollections, eMoveType&e_eMoveType)
+//{
+//	//Microsoft::UI::Xaml::Controls::TreeViewItem^ l_pParent = nullptr;
+//	unsigned int l_uiIndex = -1;
+//	Microsoft::UI::Xaml::Controls::TreeViewNode^l_pTarget = e_pTarget;
+//	while (e_pCollections->IndexOf(l_pTarget->Parent, &l_uiIndex))
+//	{
+//		l_pTarget = (Microsoft::UI::Xaml::Controls::TreeViewNode^)e_pCollections->GetAt(l_uiIndex);
+//	}
+//	if ( l_pTarget != e_pTarget)
+//	{
+//		e_eMoveType = eMoveType::eMT_PROCESS_ONE_NODE_CHANGE_PARENT;
+//	}
+//	else
+//	{
+//		e_eMoveType = eMoveType::eMT_PROCESS_PARELLEL_NODES_CHANGE_ORDER;
+//	}
+//	return l_pTarget;
+//	
+//}
+
+
+//bool GetMPDINode(cMPDINode** e_pOut);
+bool UIEditor::UITreeView::GetMPDINode(size_t* e_puiOutAddress)
+{
+	if (m_pLastIvokedTreeviewNode)
+	{
+		if (m_TreeviewNodeAndcMPDINodeMap->HasKey(m_pLastIvokedTreeviewNode))
+		{
+			*e_puiOutAddress = m_TreeviewNodeAndcMPDINodeMap->Lookup(m_pLastIvokedTreeviewNode);
+			//cMPDINode*l_pMPDINode = (cMPDINode*)l_uiAddress;
+			//*e_pOut = (cMPDINode*)l_uiAddress;
+			return true;
+		}
+	}
+	return false;
+}
+
 void UIEditor::UITreeView::m_UITreeView_DragItemsCompleted(Microsoft::UI::Xaml::Controls::TreeView^ sender, Microsoft::UI::Xaml::Controls::TreeViewDragItemsCompletedEventArgs^ args)
 {
+	auto l_Result = args->DropResult;
+	if (l_Result == Windows::ApplicationModel::DataTransfer::DataPackageOperation::Move)
+	{
+		eMoveType l_eMoveType = eMoveType::eMT_MAX;
+		if (args->Items->Size )
+		{
+			String^ l_strName = nullptr;
+			Microsoft::UI::Xaml::Controls::TreeViewNode^ l_pParent = nullptr;
+			auto l_pFirstSelectedItem = args->Items->GetAt(0);
+			auto l_strType = l_pFirstSelectedItem->GetType()->ToString();
+			if (args->NewParentItem)
+			{
+				l_pParent = (Microsoft::UI::Xaml::Controls::TreeViewNode^)args->NewParentItem;
+				l_eMoveType = eMoveType::eMT_PROCESS_NODE_CHANGE_PARENT;
+			}
+			else
+			{
+//				auto l_Parent = GetParentInCollections((Microsoft::UI::Xaml::Controls::TreeViewNode^)l_pFirstSelectedItem, args->Items, l_eMoveType);
+				l_eMoveType = eMoveType::eMT_PROCESS_NODECHANGE_ORDER;
+				l_pParent = ((Microsoft::UI::Xaml::Controls::TreeViewNode^)l_pFirstSelectedItem)->Parent;
+			}
+			l_strName = l_pParent->ToString();
+			l_strType = l_pParent->GetType()->ToString();
+			auto ooo = args->Items;
+			for (int i = 0; i < (int)args->Items->Size; ++i)
+			{
+				l_strName = args->Items->GetAt(i)->ToString();
+				OutputDebugString(l_strName->Data());
+				OutputDebugString(L"\n");
+				int a = 0;
+			}
+			int a = 0;
+		}
+	}
 	int a = 0;
 }
 
 
 void UIEditor::UITreeView::m_UITreeView_ItemInvoked(Microsoft::UI::Xaml::Controls::TreeView^ sender, Microsoft::UI::Xaml::Controls::TreeViewItemInvokedEventArgs^ args)
 {
+	m_pLastIvokedTreeviewNode = args->InvokedItem;
+	auto l_str = args->InvokedItem->ToString();
 	int a = 0;
 }
 
--- Src/FM79979Engine/UIEditor/UIEditor/UITreeView.xaml.h
+++ Src/FM79979Engine/UIEditor/UIEditor/UITreeView.xaml.h
@@ -17,7 +17,11 @@ namespace UIEditor
 	{
 	public:
 		UITreeView();
+		//cMPDINode*
+		property Windows::Foundation::Collections::IMap<Platform::Object^, size_t>^	m_TreeviewNodeAndcMPDINodeMap;
+		bool GetMPDINode(size_t *e_puiOutAddress);
 	private:
+		Platform::Object^ m_pLastIvokedTreeviewNode;
 		void m_UITreeView_DragItemsCompleted(Microsoft::UI::Xaml::Controls::TreeView^ sender, Microsoft::UI::Xaml::Controls::TreeViewDragItemsCompletedEventArgs^ args);
 		void m_UITreeView_ItemInvoked(Microsoft::UI::Xaml::Controls::TreeView^ sender, Microsoft::UI::Xaml::Controls::TreeViewItemInvokedEventArgs^ args);
 		void m_UITreeView_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
