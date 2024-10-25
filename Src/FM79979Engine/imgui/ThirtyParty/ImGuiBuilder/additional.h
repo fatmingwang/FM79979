c0c18d370adbca95e52819581d60052465d1e9d8
Fatming <osimejp@yahoo.co.jp>
2024-10-25 16:00:36 +0800

    add code

---
+++ Src/FM79979Engine/imgui/ThirtyParty/ImGuiBuilder/additional.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "../../imgui.h"
+#include <vector>
+
+class basic_obj;
+
+namespace ImGui
+{
+	void ToggleButton( const char* str_id, bool* v );
+	void PushAllColorsDark( ImGuiStyle dark );
+	void PushAllColorsCustom( ImGuiStyle custom_gui_style );
+	void PopAllColorsCustom( );
+	bool InputTextEx( const char* label, std::string* str, ImGuiInputTextFlags flags );
+	//draw selected border
+	void DrawObjBorder( ImVec2 obj_pos, ImVec2 obj_size, float distance_thickness = 5.f, ImU32 col = IM_COL32( 255, 255, 0, 255 ) );
+	void DrawObjBorder( basic_obj obj, float distance_thickness = 5.f, ImU32 col = IM_COL32( 255, 255, 0, 255 ) );
+}
---
+++ Src/FM79979Engine/imgui/ThirtyParty/ImGuiBuilder/build_config.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../../imgui.h"
+#include <string>
+#include "../../MyGui/MyGui.h"
+
+namespace im_config
+{
+	namespace window_flags
+	{
+		bool save( std::string style, ImGuiStyle custom_gui_style );
+		bool load( std::string style, ImGuiStyle& custom_gui_style );
+		void to_clipboard( ImGuiStyle& custom_gui_style );
+	}
+
+	namespace color
+	{
+		bool save( std::string style, ImGuiStyle custom_gui_style );
+		bool load( std::string style, ImGuiStyle& custom_gui_style );
+		void to_clipboard( ImGuiStyle custom_gui_style );
+		ImVec4* saved_colors( );
+	}
+
+	namespace controls
+	{
+		bool save( std::string& file, std::vector<form> forms, std::vector<basic_obj> objs );
+		bool load( std::string& file, std::vector<form>& forms, std::vector<basic_obj>& objs, int* the_ids );
+		bool create_code( std::string file_name, std::vector<form> forms, std::vector<basic_obj> objs );
+	}
+}
