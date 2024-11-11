5f7fc5642d82fd1f409d0122f1477baf6b893556
Fatming <osimejp@yahoo.co.jp>
2024-11-11 17:21:50 +0800

    minor change

---
+++ Src/FM79979Engine/EmptyProject/Win32Test/GUIForFileTransfer.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "GUIForFileTransfer.h"
+
+void cGUIForFileTransfer::FetchVersionFileList()
+{
+}
+
+cGUIForFileTransfer::cGUIForFileTransfer()
+{
+}
+
+cGUIForFileTransfer::~cGUIForFileTransfer()
+{
+}
+
+void cGUIForFileTransfer::Init()
+{
+}
+
+void cGUIForFileTransfer::Update(float e_fElpaseTime)
+{
+}
+
+void cGUIForFileTransfer::Render()
+{
+}
+
+void cGUIForFileTransfer::Destory()
+{
+}
---
+++ Src/FM79979Engine/EmptyProject/Win32Test/GUIForFileTransfer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+//a Deploy.json for file content
+//a combobox for src env
+//a listbox for target env
+
+
+//a button dor fetch env Rule.json file and all version
+// 
+//a new panel show default using version and a list box for using special rule game list(game ID and shared code version)
+//
+//a listbox show all verion folder
+// 
+//a listbox to select game
+//a drop box for set target game for use specific version
+//a button to combine 
+
+class cGUIForFileTransfer:NaemdTypedObject
+{
+	enum eEnv
+	{
+		eE_DEV = 0,
+		eE_UAT,
+		eE_SIT,
+		eE_FUN,
+		eE_PROD,
+		eE_MAX
+	};
+	struct sEnvData
+	{
+		std::string					m_steVersionFileName;
+		std::vector<std::string>	m_VersionVector;
+		//
+		std::string					m_strBackupDirectory;
+		//
+		std::string					m_strRemoteUserName;
+		std::string					m_strRemotePassword;
+	};
+	std::vector<std::string>		m_VersionVector;
+	bool							m_bDoingUpdate;
+	void							FetchVersionFileList();
+	class cMyGuiNode*				m_pRoot;
+public:
+	cGUIForFileTransfer();
+	virtual ~cGUIForFileTransfer();
+	void	Init();
+	void	Update(float e_fElpaseTime);
+	void	Render();
+	void	Destory();
+};
