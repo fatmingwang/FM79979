f18f49c58cc981ade507bf497844453c195ae9d7
Fatming <osimejp@yahoo.co.jp>
2024-11-20 17:42:42 +0800

    minor change

--- Src/FM79979Engine/MyGUIWithSSHFileTransfer/SFTPTransfer.h
+++ Src/FM79979Engine/MyGUIWithSSHFileTransfer/SFTPTransfer.h
@@ -3,6 +3,6 @@
 
 void	AssignEnvData(const char* e_strUserName, const char* e_strPW, const char* e_strIP, const int e_iPort);
 void	LibSSH2Init();
-bool	UploadFile(const char*e_strEnvName,const std::string& local_path, const std::string& remote_path);
-bool	DownloadFile(const char* e_strEnvName,const std::string& local_path, const std::string& remote_path);
+bool	UploadFileOrDirectory(const char*e_strEnvName,const std::string& e_strLocalPath, const std::string& e_strRemotePath);
+bool	DownloadFileOrDirectory(const char* e_strEnvName,const std::string& e_strLocalPath, const std::string& e_strRemotePath);
 void	LibSSH2ShutDown();
